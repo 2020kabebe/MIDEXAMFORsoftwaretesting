3a30bdd1ce1c241daef0a539f3242790555fcc02
Tuyisenge Nelyse KABEBE <151851067+2020kabebe@users.noreply.github.com>
2024-03-18 13:46:08 +0200

    Add files via upload

---
+++ StudentDao.java
@@ -0,0 +1,64 @@
+package dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.boot.jaxb.cfg.spi.JaxbCfgEventTypeEnum;
+import org.hibernate.query.Query;
+
+import model.*;
+import repo.*;
+
+public class StudentDao {
+	
+	public void saveStudent(Student student) {
+		Session ss = HibernateUtility.getSession().openSession();
+
+		Transaction tr = ss.beginTransaction();
+		try {
+			  	ss.save(student);
+			  	tr.commit();
+				ss.close();
+		   }catch(Exception e) {
+			tr.rollback();
+			e.printStackTrace();
+		}
+	}
+	
+	public List<Student> displayAllStudents() {
+		Session ss = HibernateUtility.getSession().openSession();
+		
+		try {
+			List<Student> students = ss.createQuery("from Student").list();
+			return students;
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public Student searchStudentById(String id) {
+		Session ss = HibernateUtility.getSession().openSession();
+		try {
+			Query query = ss.createQuery("select student from Student student where student.regNo = :id");
+			query.setParameter("id", id);
+			return (Student) ((org.hibernate.query.Query) query).uniqueResult();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
